12ca28eb649ac741041410a91dd8387695eaa917
janak <jaykapadiya8899@gmail.com>
2022-07-23 14:53:15 +0530

    --

--- src/main/java/com/accountmanagement/practice/Services/BankAccountService.java
+++ src/main/java/com/accountmanagement/practice/Services/BankAccountService.java
@@ -4,11 +4,13 @@ import com.accountmanagement.practice.Exceptions.AccountNotFoundException;
 
 import com.accountmanagement.practice.Exceptions.NotSufficientBalance;
 import com.accountmanagement.practice.Model.Accounts;
+import com.accountmanagement.practice.Model.Transaction;
 import com.accountmanagement.practice.Model.User;
 import com.accountmanagement.practice.Repository.AccountRepository;
 import java.util.List;
 import java.util.Optional;
 
+import com.accountmanagement.practice.Repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,6 +23,9 @@ public class BankAccountService{
 
 	private final AccountRepository accountrepository;
 	private final UserService userService;
+
+	private final TransactionRepository transactionRepository;
+
 
 	public Accounts findById(int id) throws AccountNotFoundException {
 		Optional<Accounts> accountidOptional = accountrepository.findById(id);
@@ -42,21 +47,30 @@ public class BankAccountService{
 	{
 		return accountrepository.findAll(PageRequest.of(pageNumber, pageSize));
 	}
-	public void addMoney(int id,int amount) throws AccountNotFoundException
+	public void addMoney(int id,int amount) throws RuntimeException
 	{
 		Accounts accounts = this.findById(id);
 		int newBalance = accounts.getBalance() + amount;
 		accounts.setBalance(newBalance);
 		accountrepository.save(accounts);
+		Transaction transaction = new Transaction();
+		transaction.setAccounts(accounts);
+		transaction.setAmount(amount);
+		transaction.setTransactionType("Deposited");
+		transactionRepository.save(transaction);
 	}
 	
 	public void withdraw(int id,int amount) throws AccountNotFoundException,NotSufficientBalance
 	{
 		Accounts accounts = this.findById(id);
-	    int updatedBalance = accounts.getBalance() - amount;
-	    accounts.setBalance(updatedBalance);
-
+		int updatedBalance = accounts.getBalance() - amount;
+		accounts.setBalance(updatedBalance);
 		accountrepository.save(accounts);
+		Transaction transaction = new Transaction();
+		transaction.setAccounts(accounts);
+		transaction.setAmount(amount);
+		transaction.setTransactionType("withdraw");
+		transactionRepository.save(transaction);
 	}
 	
 	public int checkBalance(int id) throws AccountNotFoundException
@@ -64,7 +78,7 @@ public class BankAccountService{
 	    Accounts accounts = this.findById(id);
 		return accounts.getBalance();
 	}
-	
+
 	public Accounts addAccount(String name, int amount, int userId)
 	{
 		User user = userService.getbyId(userId);
@@ -72,7 +86,14 @@ public class BankAccountService{
 		account.setName(name);
 		account.setBalance(amount);
 		account.setUser(user);
-		return accountrepository.save(account);
+		Accounts accounts = accountrepository.save(account);
+
+		Transaction transaction = new Transaction();
+		transaction.setAmount(amount);
+		transaction.setTransactionType("Deposited");
+		transaction.setAccounts(accounts);
+        transactionRepository.save(transaction);
+		return  accounts;
 	}
 	public List<Accounts> findByName(String name) {
 		return accountrepository.searchByName(name);
